c56dcecc8361c79cdf3cd67a82b4b2823b64f94d
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-02-18 11:22:34 -0800

    Add articles management feature with new service and UI
    Introduce `ArticlesOverviewService` for fetching and toggling articles' publish status. Add `ArticleOverview.razor` UI component for user-driven article management, incorporating `QuickGrid` for display. Update project setup with necessary service registrations and dependencies.

--- BlazingBlog.Web.Client/Program.cs
+++ BlazingBlog.Web.Client/Program.cs
@@ -1,3 +1,5 @@
+using BlazingBlog.Application.Articles;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -5,6 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient
+{
+		BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+
+builder.Services.AddScoped<IArticlesOverviewService, BlazingBlog.WebUI.Client.Features.Articles.ArticlesOverviewService>();
 
 await builder.Build().RunAsync();
