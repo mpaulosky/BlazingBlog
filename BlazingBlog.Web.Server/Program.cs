18f18c79772af1c5313bf091d3a5e5f95ebe353b
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-02-18 07:08:14 -0800

    Add infrastructure, routes, and authentication updates
    Introduced support for database connections and modularized the application architecture by integrating Application and Infrastructure layers. Enhanced routing with authorization, updated Razor components with interactive modes, and added support for TailwindCSS. Included necessary dependencies and project references for EF Core and PostgreSQL integration.

--- BlazingBlog.Web.Server/Program.cs
+++ BlazingBlog.Web.Server/Program.cs
@@ -1,27 +1,44 @@
+using BlazingBlog.Application;
+using BlazingBlog.Infrastructure;
 using BlazingBlog.Web.Server.Components;
+
+using Mapster;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents()
+		.AddInteractiveServerComponents()
+		.AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddApplication();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
 
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
 
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapRazorComponents<App>();
+
+app.MapRazorComponents<App>()
+		.AddInteractiveServerRenderMode()
+		.AddInteractiveWebAssemblyRenderMode()
+		.AddAdditionalAssemblies(typeof(BlazingBlog.Web.Client._Imports).Assembly);
 
 app.Run();
