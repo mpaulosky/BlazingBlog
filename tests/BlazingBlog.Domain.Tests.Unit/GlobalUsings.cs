77d7ead1f0bd95aad12482c32f75ead1459325c1
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-02-20 10:54:53 -0800

    Add unit tests for Article domain model
    Introduce `ArticlesTests` to verify the integrity and behavior of the `Article` domain model, covering property validation and handling of null values. Configured the test project with necessary dependencies and global usings for streamlined testing.

---
+++ tests/BlazingBlog.Domain.Tests.Unit/GlobalUsings.cs
@@ -0,0 +1,16 @@
+// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     GlobalUsings.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : BlazingBlog
+// Project Name :  BlazingBlog.Domain.Tests.Unit
+// =======================================================
+
+global using System.Diagnostics.CodeAnalysis;
+
+global using Bogus;
+
+global using FluentAssertions;
+
+global using JetBrains.Annotations;
