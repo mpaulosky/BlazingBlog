8c74acf62791e849a2eb740d06cf997ff826036a
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-02-23 12:36:30 -0800

    Add missing global usings to unit test project
    This update includes essential global usings for various namespaces required in unit tests. It improves test readability and reduces redundancy by centralizing commonly used imports.

--- tests/BlazingBlog.Application.Tests.Unit/GlobalUsings.cs
+++ tests/BlazingBlog.Application.Tests.Unit/GlobalUsings.cs
@@ -9,6 +9,28 @@
 
 global using System.Diagnostics.CodeAnalysis;
 
+global using BlazingBlog.Application.Abstractions.RequestHandling;
+global using BlazingBlog.Application.Articles.GetArticlesByCurrentUser;
+global using BlazingBlog.Application.Articles.TogglePublishArticle;
+global using BlazingBlog.Application.Authentication;
+global using BlazingBlog.Application.Exceptions;
+global using BlazingBlog.Application.Users;
+global using BlazingBlog.Domain.Abstractions;
+global using BlazingBlog.Domain.Article;
+global using BlazingBlog.Domain.Users;
+global using BlazingBlog.Infrastructure.Users;
+
+global using Bogus;
+
 global using FluentAssertions;
 
 global using JetBrains.Annotations;
+
+global using Mapster;
+
+global using MediatR;
+
+global using Microsoft.Extensions.Logging;
+
+global using NSubstitute;
+global using NSubstitute.ExceptionExtensions;
