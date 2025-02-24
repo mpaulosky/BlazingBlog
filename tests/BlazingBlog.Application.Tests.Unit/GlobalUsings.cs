e049ec1ab2e72db3fb7c2b2bfc7b5c04d71d98f5
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-02-24 10:27:35 -0800

    Update global usings in unit tests
    Added new namespace imports for `BlazingBlog.Application.Articles` and helper static methods to streamline test setup. Also removed unused `Microsoft.Extensions.Logging` to clean up the code.

--- tests/BlazingBlog.Application.Tests.Unit/GlobalUsings.cs
+++ tests/BlazingBlog.Application.Tests.Unit/GlobalUsings.cs
@@ -10,6 +10,7 @@
 global using System.Diagnostics.CodeAnalysis;
 
 global using BlazingBlog.Application.Abstractions.RequestHandling;
+global using BlazingBlog.Application.Articles;
 global using BlazingBlog.Application.Articles.GetArticlesByCurrentUser;
 global using BlazingBlog.Application.Articles.TogglePublishArticle;
 global using BlazingBlog.Application.Authentication;
@@ -19,6 +20,7 @@ global using BlazingBlog.Domain.Abstractions;
 global using BlazingBlog.Domain.Article;
 global using BlazingBlog.Domain.Users;
 global using BlazingBlog.Infrastructure.Users;
+global using static BlazingBlog.Application.Helpers.Helpers;
 
 global using Bogus;
 
@@ -29,8 +31,6 @@ global using JetBrains.Annotations;
 global using Mapster;
 
 global using MediatR;
-
-global using Microsoft.Extensions.Logging;
 
 global using NSubstitute;
 global using NSubstitute.ExceptionExtensions;
